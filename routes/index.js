d9d1f9916b5ab83ebad6e23f33317e44902accf5
diegoferesin <diegoferesin@Diegos-MacBook-Pro.local>
2023-10-14 13:16:05 -0300

    Auth feature added

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 router.get('/', (req, res) => {
-    res.send('Hello team 1 and Hello World!');
+    //#swagger.tag=['Welcome']
+    res.send('Hello Team 1 and Hello World!');
+});
+
+router.get('/logout-message', (req, res) => {
+    res.send('Logged out');
+});
+
+router.get('/login', passport.authenticate('github', (req, res) => { }));
+
+router.get('/logout', (req, res) => {
+    req.logout(function (err) {
+        if (err) { return next(err); }
+        res.redirect('/logout-message');
+    });
 });
 
 //REDIRECT TO OTHER FILES
--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
-const bodyParser = require("body-parser");
-const port = process.env.PORT || 3000;
 const app = express();
+const bodyParser = require("body-parser");
+const passport = require('passport');
+const session = require('express-session');
+const GitHubStrategy = require('passport-github').Strategy;
+const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+const port = process.env.PORT || 3000;
+
 
 const mongodb = require('./data/database');
 
 app.use(bodyParser.json());
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+}))
+app.use(passport.initialize());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
@@ -16,7 +29,48 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(cors(
+    {
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        origin: '*',
+    }
+));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/', require('./routes'));
+
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.CALLBACK_URL
+},
+    function (accessToken, refreshToken, profile, done) {
+        return done(null, profile);
+    }
+));
+
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+    return done(null, user);
+});
+
+app.get('/', (req, res) => { res.send(res.send(req.session.user != undefined ? `Logged in as ${req.session.user.displayName}` : 'Logged Out')) });
+
+app.get('/github/callback', passport.authenticate('github', {
+    failureRedirect: '/api-docs', session: false
+}),
+    (req, res) => {
+        req.session.user = req.user;
+        res.redirect('/');
+    });
+
+
+process.on('uncaughtException', (err, origin) => {
+    console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
+});
 
 mongodb.initDb((err, db) => {
     if (err) {
